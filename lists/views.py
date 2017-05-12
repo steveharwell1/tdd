cf4e52354130e64128acd3a6de16f12e2a66164d
Stephen Harwell <steveharwell@gmail.com>
2017-05-12 13:33:48 -0500

    Created a list and made each item have a list foreign key.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -11,6 +11,7 @@ def view_list(request):
     return render(request, 'lists/list.html', {'items': items})
 
 def new_list(request):
+    list_ = List.objects.create()
     new_item_text = request.POST['item_text']
-    Item.objects.create(text=new_item_text)
+    Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
