853b58d8a191dbe174c3873fa74f7999dde8bd05
Stephen Harwell <steveharwell@gmail.com>
2017-05-18 20:54:19 -0500

    settings.py now uses secret_key.txt

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-8w+-s!bwyb5hn^72%7#^_giej_nusvd0y9misi@8+^o8d1i+m'
+with open('../keys/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
