ff3c5b0c68bb3d1fcce1a4b15b08f8fd588b45c3
Stephen Harwell <steveharwell@gmail.com>
2017-05-21 17:17:17 -0500

    Have a working form class that bubbles proper error message through the web stack

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,10 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+
+from lists.forms import (DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+                         ExistingListItemForm, ItemForm)
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -97,6 +101,20 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{list_.id}/')
         self.assertIsInstance(response.context['form'], ItemForm)
         self.assertContains(response, 'name="text"')
+    
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
+        
 
 
 class HomePageTest(TestCase):
@@ -153,4 +171,3 @@ class NewListTest(TestCase):
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertIsInstance(response.context['form'], ItemForm)
-
