60933a5016b788ea3cda7da1fef840769682650f
Stephen Harwell <steveharwell@gmail.com>
2017-05-21 16:35:48 -0500

    Saving progress on duplicate item prevention. Not yet workable.

--- lists/views.py
+++ lists/views.py
@@ -14,8 +14,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            new_item_text = request.POST['text']
-            item = Item.objects.create(text=new_item_text, list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'lists/list.html', {'list': list_, 'form': form})
 
@@ -23,8 +22,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        new_item_text = request.POST['text']
-        Item.objects.create(text=new_item_text, list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {"form": form})
