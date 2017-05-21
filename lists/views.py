3308c0ed478c05ca1accdf48a6d4f728acc59d0b
Stephen Harwell <steveharwell@gmail.com>
2017-05-21 10:23:15 -0500

    use form in all views, back to working state

--- lists/views.py
+++ lists/views.py
@@ -10,17 +10,14 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    form = ItemForm()
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
             new_item_text = request.POST['text']
             item = Item.objects.create(text=new_item_text, list=list_)
             return redirect(list_)
-        else:
-            return render(request, 'lists/list.html', {'list': list_, 'form': form})
-    else:
-        pass
-    return render(request, 'lists/list.html', {'list': list_, 'form': ItemForm()})
+    return render(request, 'lists/list.html', {'list': list_, 'form': form})
 
 def new_list(request):
     form = ItemForm(data=request.POST)
