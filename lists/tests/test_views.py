e0d744feb167137bb39649404010afd5c029f8b6
Stephen Harwell <steveharwell@gmail.com>
2017-05-21 09:43:53 -0500

    refactored listview

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -65,8 +65,14 @@ class ListViewTest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lists/list.html')
-        expected_error = escape("You can't have an empty list item")
+        expected_error = escape(EMPTY_ITEM_ERROR)
         self.assertContains(response, expected_error)
+    
+    def test_display_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
 
 
 class HomePageTest(TestCase):
@@ -103,10 +109,24 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'lists/home.html')
-        expected_error = escape("You can't have an empty list item")
+        expected_error = escape(EMPTY_ITEM_ERROR)
         self.assertContains(response, expected_error)
 
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+    
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lists/home.html')
+    
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+    
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+
--- lists/views.py
+++ lists/views.py
@@ -10,28 +10,24 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
     if request.method == 'POST':
-        try:
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
             new_item_text = request.POST['text']
-            item = Item(text=new_item_text, list=list_)
-            item.full_clean()
-            item.save()
+            item = Item.objects.create(text=new_item_text, list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
-    return render(request, 'lists/list.html', {'list': list_, "error": error})
+        else:
+            return render(request, 'lists/list.html', {'list': list_, 'form': form})
+    else:
+        pass
+    return render(request, 'lists/list.html', {'list': list_, 'form': ItemForm()})
 
 def new_list(request):
-    list_ = List.objects.create()
-    new_item_text = request.POST['text']
-    item = Item.objects.create(text=new_item_text, list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'lists/home.html', {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        new_item_text = request.POST['text']
+        Item.objects.create(text=new_item_text, list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'lists/home.html', {"form": form})
