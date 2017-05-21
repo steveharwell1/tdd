4a0e400fe41e28cb094373a4669dad47ff76d99a
Stephen Harwell <steveharwell@gmail.com>
2017-05-21 08:21:45 -0500

    changed static form in template over to forms.py

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -75,6 +75,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
 
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+
 
 class NewListTest(TestCase):
 
