7ca5990bcb6bd39bff323293dcd2460c6c9cce01
Stephen Harwell <steveharwell@gmail.com>
2017-05-22 11:03:23 -0500

    New Ft for making duplication error disappear once action is taken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,6 +10,8 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
     """"""
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
 
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
@@ -66,6 +68,28 @@ class ItemValidationTest(FunctionalTest):
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             DUPLICATE_ITEM_ERROR
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Eith starts a list and causes a validation error:
+        TEXT = 'Banter too thick'
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(TEXT)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(f'1: {TEXT}')
+        self.get_item_input_box().send_keys(TEXT)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is please to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
